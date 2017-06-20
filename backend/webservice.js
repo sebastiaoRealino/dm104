05943c994476ccec589b14bae07ff6d75ed93baa
sebastiaoRealino <sebastiaorealino@yahoo.com.br>
2017-06-20 05:55:52 -0300

    Fake cars creation working

--- backend/webservice.js
+++ backend/webservice.js
@@ -8,7 +8,7 @@ var db = require('./db');
 url_pattern = '/api/v1/'
 
 var db = require('./db');
-app.get('/save', function (req, res) {
+app.get('/save_user_fake', function (req, res) {
     var post = { name: 'me', gender: 'M', user_type: 'SELLER' };
     db.query('INSERT INTO users SET ?', post, function (err, result) {
         if (err) throw err;
@@ -18,9 +18,23 @@ app.get('/save', function (req, res) {
         }
     });
 });
+app.get('/save_car_fake', function (req, res) {
+    var post = { model: '1998', color: 'red', price: '500', is_sold: false, sold_date: '1991-02-04' };
+    db.query('INSERT INTO cars SET ?', post, function (err, result) {
+        if (err) throw err;
+        else {
+            console.log(result.insertId);
+            res.send('Carro salvo com sucesso!');
+        }
+    });
+});
 
 app.get(url_pattern + 'cars', function (req, res) {
-    res.send('listagem de carros');
+    db.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });    
 });
 
 app.get(url_pattern + 'users', function (req, res) {
